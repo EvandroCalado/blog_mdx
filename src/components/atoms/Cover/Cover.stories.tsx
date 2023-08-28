3e9032802a2837cc1cd5ef83454ccfda2edfee47
evandrocalado <evandrocalado07@gmail.com>
2023-08-28 14:16:11 -0300

    :feat: create Cover component with stories

---
+++ src/components/atoms/Cover/Cover.stories.tsx
@@ -0,0 +1,18 @@
+import Cover, { CoverProps } from './Cover';
+import { Meta, StoryFn } from '@storybook/react';
+
+export default {
+  title: 'Atoms/Cover',
+  component: Cover,
+  args: {
+    cover:
+      'https://images.unsplash.com/photo-1537884944318-390069bb8665?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    coverResource: 'unsplash',
+    coverResourceUrl: 'https://unsplash.com/pt-br',
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as Meta;
+
+export const Default: StoryFn<CoverProps> = (args) => <Cover {...args} />;
---
+++ src/components/atoms/Cover/Cover.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export type CoverProps = {
+  cover: string;
+  coverResource: string;
+  coverResourceUrl: string;
+};
+
+export default function Cover({
+  cover,
+  coverResource,
+  coverResourceUrl,
+}: CoverProps) {
+  return (
+    <div className="text-center">
+      <div className="relative mb-4 h-[470px] w-full">
+        <Image src={cover} alt="cover" fill className="object-cover" />
+      </div>
+      <span className="mr-2">Imagem por:</span>
+      <Link
+        href={coverResourceUrl}
+        target="_blank"
+        className="font-bold capitalize underline duration-200 hover:brightness-75"
+      >
+        {coverResource}
+      </Link>
+    </div>
+  );
+}
