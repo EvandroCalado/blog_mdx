81e528151d028685a11694a8e331d5a412eb4804
evandrocalado <evandrocalado07@gmail.com>
2023-08-28 14:17:11 -0300

    :feat: create Card component with stories

---
+++ src/components/molecules/Card/Card.stories.tsx
@@ -0,0 +1,19 @@
+import Card, { CardProps } from './card';
+import { Meta, StoryFn } from '@storybook/react';
+
+export default {
+  title: 'Molecules/Card',
+  component: Card,
+  args: {
+    title: 'Card title',
+    description:
+      'React é uma biblioteca JavaScript de código aberto para construir interfaces de usuário (IU). Ela é uma biblioteca declarativa, o que significa que você descreve como sua IU deve ser, e o React se encarrega de renderizá-la.',
+    cover:
+      'https://images.unsplash.com/photo-1537884944318-390069bb8665?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    date: '2023-08-26',
+    name: 'Evandro Calado',
+    image: 'https://avatars.githubusercontent.com/u/110628201?v=4',
+  },
+} as Meta;
+
+export const Default: StoryFn<CardProps> = (args) => <Card {...args} />;
