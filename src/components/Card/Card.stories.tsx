c39ac89cff1683c10b83bbc20c0c9adc4cc339e0
evandrocalado <evandrocalado07@gmail.com>
2023-09-01 08:47:17 -0300

    :feat: add categories, avatar and validations

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -1,8 +1,9 @@
-import Card, { CardProps } from './card';
 import { Meta, StoryFn } from '@storybook/react';
+import { Post } from 'contentlayer/generated';
+import Card from './card';
 
 export default {
-  title: 'Molecules/Card',
+  title: 'Components/Card',
   component: Card,
   args: {
     title: 'Card title',
@@ -11,9 +12,13 @@ export default {
     cover:
       'https://images.unsplash.com/photo-1537884944318-390069bb8665?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
     date: '2023-08-26',
-    name: 'Evandro Calado',
-    image: 'https://avatars.githubusercontent.com/u/110628201?v=4',
+    url: '/',
+    categories: [{ name: 'javascript' }, { name: 'react' }],
+    author: {
+      name: 'Evandro Calado',
+      image: 'https://avatars.githubusercontent.com/u/110628201?v=4',
+    },
   },
 } as Meta;
 
-export const Default: StoryFn<CardProps> = (args) => <Card {...args} />;
+export const Default: StoryFn<Post> = (args) => <Card {...args} />;
---
+++ src/components/Card/card.tsx
@@ -0,0 +1,54 @@
+import { Post } from 'contentlayer/generated';
+import Image from 'next/image';
+import Avatar from '../Avatar/Avatar';
+import Categories from '../Categories/Categories';
+import LinkButton from '../LinkButton/LinkButton';
+
+export default function Card({
+  title,
+  description,
+  cover,
+  date,
+  url,
+  author,
+  categories,
+}: Post) {
+  if (!author || !author.name || !author.image)
+    return <div className="w-full flex-1">Post sem Autor</div>;
+
+  if (!categories)
+    return <div className="w-full flex-1">Post sem Categorias</div>;
+
+  return (
+    <div className="max-w-sm rounded-lg border border-theme-400 bg-theme-100 shadow-lg shadow-theme-300 dark:border-theme-600 dark:bg-theme-950 dark:shadow-theme-950">
+      <div className="relative h-56 w-full">
+        <Image
+          className="rounded-t-lg object-cover"
+          src={cover}
+          alt="Cover"
+          fill
+        />
+      </div>
+      <div className="p-5">
+        <div>
+          <Categories categories={categories} className="mb-2" />
+        </div>
+        <h5 className="mb-2 line-clamp-1 text-2xl font-bold tracking-tight">
+          {title}
+        </h5>
+        <p className="mb-3 line-clamp-3 font-normal text-theme-700 dark:text-zinc-400">
+          {description}
+        </p>
+        <div className="flex items-center justify-between">
+          <LinkButton href={url}>Ler mais</LinkButton>
+          <Avatar
+            name={author.name}
+            image={author.image}
+            date={date}
+            className="space-x-1"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
