231643d6915a6c29dbda38d1bf09f26efee8690f
evandrocalado <evandrocalado07@gmail.com>
2023-09-01 08:53:40 -0300

    :feat: create Toggle component with stories

---
+++ src/components/Toggle/Toggle.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { icons } from 'src/icons';
+
+const initialDarkMode =
+  typeof window !== 'undefined' && localStorage.getItem('theme') === 'dark';
+export default function Toggle() {
+  const [isDarkMode, setIsDarkMode] = useState(initialDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = 'light';
+    }
+  }, [isDarkMode]);
+
+  return (
+    <button
+      onClick={() => setIsDarkMode(!isDarkMode)}
+      type="button"
+      className="inline-flex items-center rounded-lg border border-theme-700 p-1.5 text-theme-700 duration-200 hover:bg-theme-200 dark:border-theme-300 dark:text-theme-300 dark:hover:bg-theme-990"
+    >
+      {isDarkMode ? (
+        <span className="[&>svg]:w-6">{icons.dark}</span>
+      ) : (
+        <span className="[&>svg]:w-6">{icons.light}</span>
+      )}
+    </button>
+  );
+}
