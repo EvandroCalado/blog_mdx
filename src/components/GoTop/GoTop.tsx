4a864c6f55ea4d76eea0a09020fad4fd1276fe97
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-06 09:49:20 -0300

    :styles: chage styles

--- src/components/GoTop/GoTop.tsx
+++ src/components/GoTop/GoTop.tsx
@@ -6,7 +6,7 @@ export default function GoTop() {
   return (
     <button
       onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-      className="sticky bottom-8 right-8 ml-auto flex h-12 w-12 items-center justify-center rounded-full bg-theme-500 text-sm font-medium text-theme-100 shadow-lg shadow-theme-950 outline-none duration-200 hover:bg-theme-600 active:bg-theme-700 dark:bg-theme-400 dark:text-theme-950 dark:hover:bg-theme-300 dark:active:bg-theme-500 [&>svg]:w-5 [&>svg]:duration-200 [&>svg]:hover:animate-bounce"
+      className="sticky bottom-8 ml-auto mr-8 flex items-center rounded-lg border border-theme-900 bg-theme-100 p-2.5  shadow-lg duration-200 hover:bg-theme-200 active:bg-theme-300 dark:border-theme-300 dark:bg-theme-900 dark:text-theme-300 dark:hover:bg-theme-950 dark:active:bg-theme-990 [&>svg]:w-5 [&>svg]:duration-200 [&>svg]:hover:animate-bounce"
     >
       {icons.arrowUp}
     </button>
