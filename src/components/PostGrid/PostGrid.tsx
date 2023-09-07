daf1427f45aaf36a01a0aa755620ce190c2a9feb
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-06 23:16:47 -0300

    :styles: add margin dynamic

--- src/components/PostGrid/PostGrid.tsx
+++ src/components/PostGrid/PostGrid.tsx
@@ -1,13 +1,20 @@
 import { Post } from 'contentlayer/generated';
+import { twMerge } from 'tailwind-merge';
 import Card from '../Card/card';
 
 export type PostGridProps = {
   posts: Post[];
+  className?: string;
 };
 
-export default function PostGrid({ posts }: PostGridProps) {
+export default function PostGrid({ posts, className }: PostGridProps) {
   return (
-    <div className="mx-auto my-12 grid max-w-6xl grid-cols-1 gap-4 px-4 sm:grid-cols-2 md:grid-cols-3">
+    <div
+      className={twMerge(
+        'mx-auto my-8 grid max-w-6xl grid-cols-1 gap-4 px-4 sm:grid-cols-2 md:grid-cols-3',
+        className,
+      )}
+    >
       {posts.map((post) => (
         <Card key={post._id} {...post} />
       ))}
